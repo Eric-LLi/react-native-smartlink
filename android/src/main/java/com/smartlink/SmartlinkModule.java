c51809e19489ba55dbce58be6f430c1de4a19aa6
Eric-LLi <ericli1116@gmail.com>
2019-11-04 02:15:30 +1100

    Connect Wifi

--- android/src/main/java/com/smartlink/SmartlinkModule.java
+++ android/src/main/java/com/smartlink/SmartlinkModule.java
@@ -12,10 +12,7 @@ import com.facebook.react.bridge.Arguments;
 import androidx.annotation.Nullable;
 
 import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
-import android.net.NetworkRequest;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiConfiguration;
@@ -23,7 +20,6 @@ import android.net.wifi.WifiConfiguration;
 import com.hiflying.smartlink.ISmartLinker;
 import com.hiflying.smartlink.OnSmartLinkListener;
 import com.hiflying.smartlink.SmartLinkedModule;
-import com.hiflying.smartlink.v3.SnifferSmartLinker;
 import com.hiflying.smartlink.v7.MulticastSmartLinker;
 
 import android.content.Context;
@@ -38,6 +34,7 @@ class FailureCodes {
 
 class EventName {
 	static String SL_CONNECT = "SL_Connect";
+	static String AP_CONNECT = "AP_Connect";
 }
 
 public class SmartlinkModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
@@ -46,9 +43,12 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 	private WifiManager wifiManager;
 	private ConnectivityManager connectivityManager;
 	private static ISmartLinker mSmartLinker;
-	private boolean mIsConncting = false;
-	private static OnSmartLinkListener listener;
-	private Promise mPromise;
+	private static boolean slIsConnecting = false;
+	private static boolean apIsConnecting = false;
+	private static OnSmartLinkListener slListener;
+	private static Promise mPromise;
+	private static String currentSSID;
+	private static String currentPwd;
 
 	public SmartlinkModule(ReactApplicationContext reactContext) {
 		super(reactContext);
@@ -57,7 +57,7 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 				.getSystemService(Context.WIFI_SERVICE);
 		connectivityManager = (ConnectivityManager) getReactApplicationContext().getApplicationContext()
 				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		listener = new OnSmartLinkListener() {
+		slListener = new OnSmartLinkListener() {
 			@Override
 			public void onLinked(SmartLinkedModule smartLinkedModule) {
 				Log.e("Connected", "Connected");
@@ -67,7 +67,7 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 				params.putString("mac", smartLinkedModule.getMac());
 				params.putString("ip", smartLinkedModule.getIp());
 				sendEvent(EventName.SL_CONNECT, params);
-				mPromise.resolve("");
+				mPromise.resolve(true);
 			}
 
 			@Override
@@ -91,14 +91,30 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 	}
 
 	private String errorFromCode(int errorCode) {
-		return "ErrorCode: " + errorCode;
+		switch (errorCode) {
+			case 1: {
+				return "SYSTEM_ADDED_CONFIG_EXISTS";
+			}
+			case 2: {
+				return "FAILED_TO_CONNECT";
+			}
+			case 3: {
+				return "FAILED_TO_ADD_CONFIG";
+			}
+			case 4: {
+				return "FAILED_TO_BIND_CONFIG";
+			}
+			default: {
+				return "unknown error";
+			}
+		}
 	}
 
 	private void stopSLConnect() {
 		if (mSmartLinker != null) {
 			mSmartLinker.setOnSmartLinkListener(null);
 			mSmartLinker.stop();
-			mIsConncting = false;
+			slIsConnecting = false;
 		}
 	}
 
@@ -135,6 +151,47 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 		return ssid;
 	}
 
+	private WifiConfiguration createWifiConfiguration(String ssid, String passphrase) {
+		WifiConfiguration configuration = new WifiConfiguration();
+		configuration.SSID = String.format("\"%s\"", ssid);
+
+		if (passphrase.equals("")) {
+			configuration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+		} else { // WPA/WPA2
+			configuration.preSharedKey = "\"" + passphrase + "\"";
+		}
+
+		if (!wifiManager.isWifiEnabled()) {
+			wifiManager.setWifiEnabled(true);
+		}
+		return configuration;
+	}
+
+	private void connect_wifi_secure(String ssid, String passphrase) throws Exception {
+
+		WifiConfiguration configuration = createWifiConfiguration(ssid, passphrase);
+		int networkId = wifiManager.addNetwork(configuration);
+
+		if (networkId != -1) {
+			// Enable it so that android can connect
+			wifiManager.disconnect();
+			boolean success = wifiManager.enableNetwork(networkId, true);
+			if (!success) {
+				throw new Exception(errorFromCode(FailureCodes.FAILED_TO_ADD_CONFIG));
+			}
+			success = wifiManager.reconnect();
+			if (!success) {
+				throw new Exception(errorFromCode(FailureCodes.FAILED_TO_CONNECT));
+			}
+			boolean connected = pollForValidSSSID(10, ssid);
+			if (!connected) {
+				throw new Exception(errorFromCode(FailureCodes.FAILED_TO_CONNECT));
+			}
+		} else {
+			throw new Exception(errorFromCode(FailureCodes.FAILED_TO_ADD_CONFIG));
+		}
+	}
+
 	@Override
 	public String getName() {
 		return "Smartlink";
@@ -152,7 +209,7 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 
 	@Override
 	public void onHostDestroy() {
-		if (mIsConncting) {
+		if (slIsConnecting) {
 			stopSLConnect();
 		}
 	}
@@ -162,15 +219,15 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 		if (mSmartLinker != null) {
 			stopSLConnect();
 		}
-		if (!mIsConncting) {
+		if (!slIsConnecting) {
 			try {
 				mPromise = promise;
 				mSmartLinker = MulticastSmartLinker.getInstance();
-				mIsConncting = true;
-				mSmartLinker.setOnSmartLinkListener(listener);
+				slIsConnecting = true;
+				mSmartLinker.setOnSmartLinkListener(slListener);
 				mSmartLinker.start(this.reactContext, ssid.trim(), pwd.trim());
 			} catch (Exception err) {
-				mIsConncting = false;
+				slIsConnecting = false;
 				promise.reject(err);
 			}
 		}
@@ -186,7 +243,7 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 		if (mSmartLinker != null) {
 			stopSLConnect();
 		}
-		promise.resolve("");
+		promise.resolve(true);
 	}
 
 	@ReactMethod
@@ -197,52 +254,36 @@ public class SmartlinkModule extends ReactContextBaseJavaModule implements Lifec
 	@ReactMethod
 	public void Get_SSID(Promise promise) {
 		String ssid = this.getWifiSSID();
-
 		promise.resolve(ssid);
 	}
 
 	@ReactMethod
-	public void Connect_WiFi_Secure(String ssid, String pwd, Boolean isWEP, Promise promise) {
-		WifiConfiguration configuration = new WifiConfiguration();
-		configuration.SSID = String.format("\"%s\"", ssid);
-
-		if (pwd.equals("")) {
-			configuration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-		} else if (isWEP) {
-			configuration.wepKeys[0] = "\"" + pwd + "\"";
-			configuration.wepTxKeyIndex = 0;
-			configuration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-			configuration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
-		} else { // WPA/WPA2
-			configuration.preSharedKey = "\"" + pwd + "\"";
-		}
-
-		if (!wifiManager.isWifiEnabled()) {
-			wifiManager.setWifiEnabled(true);
-		}
-
-		int networkId = wifiManager.addNetwork(configuration);
-
-		if (networkId != -1) {
-			wifiManager.disconnect();
-			boolean success = wifiManager.enableNetwork(networkId, true);
-			if (!success) {
-				promise.reject(errorFromCode(FailureCodes.FAILED_TO_ADD_CONFIG), "Failed to add " +
-						"config");
-				return;
+	public void Connect_WiFi(final String ssid, final Promise promise) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					connect_wifi_secure(ssid, "");
+					promise.resolve(true);
+				} catch (Exception err) {
+					promise.reject(null, err.getMessage());
+				}
 			}
-			success = wifiManager.reconnect();
-			if (!success) {
-				promise.reject(errorFromCode(FailureCodes.FAILED_TO_CONNECT), "Failed to connect " +
-						"WiFi");
-				return;
+		});
+	}
+
+	@ReactMethod
+	public void Connect_WiFi_Secure(final String ssid, final String passphrase, final Promise promise) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					connect_wifi_secure(ssid, passphrase);
+					promise.resolve(true);
+				} catch (Exception err) {
+					promise.reject(null, err.getMessage());
+				}
 			}
-			boolean connected = pollForValidSSSID(10, ssid);
-			if (!connected) {
-				promise.reject(errorFromCode(FailureCodes.FAILED_TO_CONNECT), "Failed to connect " +
-						"WiFi");
-				return;
-			}
-		}
+		}).start();
 	}
 }
--- ios/Smartlink.m
+++ ios/Smartlink.m
@@ -6,8 +6,11 @@
 
 @implementation Smartlink
 
-BOOL isconnecting = false;
+static BOOL isconnecting = false;
 BOOL v3xSupport= false;
+static NSString *currentSSID;
+static NSString *currentPwd;
+
 NSString * userStr= @"";
 HFSmartLink * smtlk;
 
@@ -15,7 +18,7 @@ RCT_EXPORT_MODULE()
 
 - (dispatch_queue_t)methodQueue
 {
-  return dispatch_queue_create("com.facebook.React.AsyncLocalStorageQueue", DISPATCH_QUEUE_SERIAL);
+    return dispatch_queue_create("com.facebook.React.AsyncLocalStorageQueue", DISPATCH_QUEUE_SERIAL);
 }
 
 RCT_EXPORT_METHOD(SL_Connect:(NSString *)ssid pwd:(NSString *)pwd
@@ -27,32 +30,32 @@ RCT_EXPORT_METHOD(SL_Connect:(NSString *)ssid pwd:(NSString *)pwd
     smtlk.waitTimers = 30;
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-      // Call long-running code on background thread
-      if(!isconnecting){
-              isconnecting = true;
-              [smtlk startWithSSID:ssid Key:pwd UserStr:userStr withV3x:v3xSupport processblock: ^(NSInteger pro) {
-                              } successBlock:^(HFSmartLinkDeviceInfo *dev) {
-                                  NSDictionary * device = @{
-                                      @"mac": dev.mac,
-                                      @"ip" : dev.ip
-                                  };
-                                  resolve(device);
-                              } failBlock:^(NSString *failmsg) {
-                                  reject(@"error", failmsg, nil);
-                              } endBlock:^(NSDictionary *deviceDic) {
-                                  isconnecting  = false;
-                              }
-               ];
-          } else {
-              [smtlk stopWithBlock:^(NSString *stopMsg, BOOL isOk) {
-                  if(isOk){
-                      isconnecting  = false;
-                      reject(@"error", @"Please try again...", nil);
-                  }else{
-                      reject(@"error", stopMsg, nil);
-                  }
-              }];
-          }
+        // Call long-running code on background thread
+        if(!isconnecting){
+            isconnecting = true;
+            [smtlk startWithSSID:ssid Key:pwd UserStr:userStr withV3x:v3xSupport processblock: ^(NSInteger pro) {
+            } successBlock:^(HFSmartLinkDeviceInfo *dev) {
+                NSDictionary * device = @{
+                    @"mac": dev.mac,
+                    @"ip" : dev.ip
+                };
+                resolve(device);
+            } failBlock:^(NSString *failmsg) {
+                reject(@"error", failmsg, nil);
+            } endBlock:^(NSDictionary *deviceDic) {
+                isconnecting  = false;
+            }
+             ];
+        } else {
+            [smtlk stopWithBlock:^(NSString *stopMsg, BOOL isOk) {
+                if(isOk){
+                    isconnecting  = false;
+                    reject(@"error", @"Please try again...", nil);
+                }else{
+                    reject(@"error", stopMsg, nil);
+                }
+            }];
+        }
     });
 }
 
@@ -84,49 +87,51 @@ RCT_EXPORT_METHOD(isAvailableConnectWiFi:
     resolve(@[available]);
 }
 
-RCT_EXPORT_METHOD(Connect_WiFi:(NSString*)ssid
-                  bindNetwork:(BOOL)bindNetwork //Ignored
+RCT_EXPORT_METHOD(Connect_WiFi_Secure:(NSString*)ssid
+                  withPassphrase:(NSString*)passphrase
                   connectResolver:(RCTPromiseResolveBlock)resolve
                   connectRejecter:(RCTPromiseRejectBlock)reject)
 {
     if (@available(iOS 11.0, *)) {
-        NEHotspotConfiguration* configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid];
-        configuration.joinOnce = !bindNetwork;
-
-        [[NEHotspotConfigurationManager sharedManager] applyConfiguration:configuration completionHandler:^(NSError * _Nullable error) {
-            if (error != nil) {
-                reject(@"Error", @"Error while configuring WiFi", nil);
-            } else {
-                resolve(@"");
-            }
-        }];
-
+        currentSSID = ssid;
+        currentPwd = passphrase;
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            NEHotspotConfiguration* configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid passphrase:passphrase isWEP:false];
+            configuration.joinOnce = true;
+            
+            [[NEHotspotConfigurationManager sharedManager] applyConfiguration:configuration completionHandler:^(NSError * _Nullable error) {
+                if (error != nil) {
+                    NSString * msg =[error localizedDescription];
+                    NSLog(@"%@",msg);
+                    reject(@"Error", msg, nil);
+                } else {
+                    resolve(@"Done");
+                }
+            }];
+        });
     } else {
         reject(@"Error", @"Not supported in iOS<11.0", nil);
     }
 }
 
-RCT_EXPORT_METHOD(Connect_WiFi_Secure:(NSString*)ssid
-                  withPassphrase:(NSString*)passphrase
-                  isWEP:(BOOL)isWEP
-                  bindNetwork:(BOOL)bindNetwork
+RCT_EXPORT_METHOD(Connect_WiFi:(NSString*)ssid
                   connectResolver:(RCTPromiseResolveBlock)resolve
                   connectRejecter:(RCTPromiseRejectBlock)reject)
 {
-    
-    if (@available(iOS 11.0, *)) {
-        NEHotspotConfiguration* configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid passphrase:passphrase isWEP:isWEP];
-        configuration.joinOnce = !bindNetwork;
+    if (@available(iOS 11.0, *)) { dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NEHotspotConfiguration* configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid];
+        configuration.joinOnce = true;
         
         [[NEHotspotConfigurationManager sharedManager] applyConfiguration:configuration completionHandler:^(NSError * _Nullable error) {
             if (error != nil) {
-                reject(@"", @"Error while connecting WiFi", error);
-//                callback(@[[error localizedDescription]]);
+                NSString * msg =[error localizedDescription];
+                NSLog(@"%@",msg);
+                reject(@"Error", msg, nil);
             } else {
-                resolve(@"");
+                resolve(@"Done");
             }
         }];
-        
+    });
     } else {
         reject(@"Error", @"Not supported in iOS<11.0", nil);
     }
@@ -136,19 +141,28 @@ RCT_REMAP_METHOD(Get_SSID,
                  connectResolver:(RCTPromiseResolveBlock)resolve
                  connectRejecter:(RCTPromiseRejectBlock)reject)
 {
-    NSString *kSSID = (NSString*) kCNNetworkInfoKeySSID;
+    NSString *ssid = [self get_ssid];
+    if(ssid != nil){
+        resolve(ssid);
+    }else{
+        reject(@"Error",@"Cannot detect SSID", nil);
+    }
+}
 
+- (NSString *) get_ssid {
+    NSString *ssid = nil;
+    
+    NSString *kSSID = (NSString*) kCNNetworkInfoKeySSID;
+    
     NSArray *ifs = (__bridge_transfer id)CNCopySupportedInterfaces();
     for (NSString *ifnam in ifs) {
         NSDictionary *info = (__bridge_transfer id)CNCopyCurrentNetworkInfo((__bridge CFStringRef)ifnam);
-        NSString *ssid = info[kSSID];
-        if (ssid) {
-            resolve(ssid);
-            return;
+        if (info[kSSID]) {
+            ssid = info[kSSID];
         }
     }
-
-    reject(@"Error",@"Cannot detect SSID", nil);
+    
+    return ssid;
 }
 
 @end
